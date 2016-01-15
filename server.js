574b86eb02b60299ebfdf130dfd9adf6d226d1f1
Monu <prankum2100@berkeley.edu>
2016-01-14 17:33:28 -0800

    todo/id get requests

--- server.js
+++ server.js
@@ -1,10 +1,42 @@
 var express = require("express");
 var app = express();
-
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: "Meet mom for lunch",
+	completed: false
+}, {
+	id: 2,
+	description: "Go to market",
+	completed: false
+}, {
+	id: 3,
+	description: "Play basketball",
+	completed: true
+}];
 
 app.get("/", function (req, res) {
 	res.send("TODO API Root");
+});
+
+// GET /todos
+app.get("/todos", function (req, res) {
+	res.json(todos);
+});
+// GET /todos/:id
+app.get("/todos/:id", function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = undefined;
+	todos.forEach(function (todo) {
+		if (todo.id === todoId) {
+			matchedTodo = todo;
+		}
+	});
+	if (!matchedTodo) {
+		res.status(404).send();
+	} else {
+		res.json(matchedTodo);
+	}
 });
 
 app.listen(PORT, function () {
