a76deb2f9228c4ee399bd9808bc8bc6d957884e4
Monu <prankum2100@berkeley.edu>
2016-01-15 19:28:17 -0800

    convert CRUD requests to use current user todos only

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ app.get("/todos",
 		middleware.requireAuthentication,
 		function(req, res) {
 	var query = req.query;
-	var where = {};
+	var where = {
+		userId: req.user.get("id")
+	};
 
 	if (query.hasOwnProperty("completed")) {
 		if (query.completed === "true") {
@@ -52,7 +54,12 @@ app.get("/todos/:id",
 		function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
-	db.todo.findById(todoId).then(function (todo) {
+	db.todo.findOne({
+		where: {
+			userId: req.user.get("id"),
+			id: todoId
+		}
+	}).then(function (todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
@@ -89,7 +96,8 @@ app.delete("/todos/:id",
 
 	db.todo.destroy({
 		where: {
-			id: deleteId
+			id: deleteId,
+			userId: req.user.get("id")
 		}
 	}).then(function (rowsDeleted)  {
 		if (rowsDeleted === 0) {
@@ -122,7 +130,12 @@ app.put("/todos/:id",
 		attr.description = body.description;
 	}
 
-	db.todo.findById(todoId).then(function (todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get("id")
+		}
+	}).then(function (todo) {
 		if (todo) {
 			todo.update(attr).then(function (todo) {
 				res.json(todo.toJSON());
